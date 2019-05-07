48a097cae65f9b5010e0633fd8a3e943f510a710
Vamshik Shetty <vamshikdshetty@gmail.com>
2019-05-07 12:33:35 +0530

    add setup sh script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pip install lxml>=4.3.0
+pip install pyVmomi>=4.3.0
+pip install suds-jurko>=3.0
+
+pip install https://github.com/vmware/vsphere-automation-sdk-python/blob/master/lib/vapi-runtime/vapi_runtime-2.10.2-py2.py3-none-any.whl?raw=true
+pip install https://github.com/vmware/vsphere-automation-sdk-python/blob/master/lib/vapi-common-client/vapi_common_client-2.10.2-py2.py3-none-any.whl?raw=true
+pip install https://github.com/vmware/vsphere-automation-sdk-python/blob/master/lib/vapi-client-bindings/vapi_client_bindings-1.5.0-py2.py3-none-any.whl?raw=true
+
+pip install https://github.com/vmware/vsphere-automation-sdk-python/blob/master/lib/nsx-policy-python-sdk/nsx_policy_python_sdk-2.3.0.0.2.11053635-py2.py3-none-any.whl?raw=true
+pip install https://github.com/vmware/vsphere-automation-sdk-python/blob/master/lib/nsx-python-sdk/nsx_python_sdk-2.3.0.0.2.11053635-py2.py3-none-any.whl?raw=true
+
+pip install https://github.com/vmware/vsphere-automation-sdk-python/blob/master/lib/vmc-app-python-sdk/vmc_app_python_sdk-2.3.0.0.2.11053635-py2.py3-none-any.whl?raw=true
+pip install https://github.com/vmware/vsphere-automation-sdk-python/blob/master/lib/vmc-client-bindings/vmc_client_bindings-1.3.1-py2.py3-none-any.whl?raw=true
